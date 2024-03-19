288a379b019a7328683bf27704c5efdfc7befa7b
hocvv <hocvanvu1999@gmail.com>
2024-03-19 10:11:51 +0700

    add marker

--- app/MapComponent.tsx
+++ app/MapComponent.tsx
@@ -19,7 +19,13 @@ function MapComponent() {
         center: new naver.maps.LatLng(37.3595704, 127.105399),
         zoom: 10,
       };
+
       const map = new naver.maps.Map("map", mapOptions);
+
+      const marker = new naver.maps.Marker({
+        position: new naver.maps.LatLng(37.3595704, 127.105399),
+        map: map,
+      });
     };
 
     if (window.naver) {
